0cc63be42fde1b14c8b12c3181227c9f70cb9dc0
rudranag <rudranag.katroju@gmail.com>
2024-11-11 20:38:45 +0530

    commented internal ips

--- DjTodos/settings.py
+++ DjTodos/settings.py
@@ -164,9 +164,9 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
 
 
 ############################  React related stuff ####################
